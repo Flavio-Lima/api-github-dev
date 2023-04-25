05bc8d954652f87d663bb47bff0673ed954c6e62
Flavio Lima <flavio.lima981@gmail.com>
2023-04-25 09:04:14 -0300

    adiciona dados os usuario

--- src/pages/RepositoriesPage/Profile/index.jsx
+++ src/pages/RepositoriesPage/Profile/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
-import {Container, Header, Avatar, Login, Name} from './styles'
+import {MdGroup, MdLocationCity, MdWork, MdLink} from 'react-icons/md'
+
+import {Container, Header, Avatar, Login, Name, Inner, Data} from './styles'
 
 function Profile() {
   return (
@@ -10,6 +12,28 @@ function Profile() {
         <Login>flaviolima</Login>
         <Name>Flávo Lima</Name>
       </Header>
+
+      <Inner>
+        <Data>
+          <MdGroup size={20} />
+          30&nbsp;<i>seguidores</i>&nbsp;&middot;&nbsp;10&nbsp;<i>seguindo</i>
+        </Data>
+
+        <Data>
+          <MdWork size={20} />
+          Odisseia do programador
+        </Data>
+
+        <Data>
+          <MdLocationCity size={20} />
+          Nova Friburgo
+        </Data>
+
+        <Data>
+          <MdLink size={20} />
+          <a href="https://devsamurai.com.br/">devsamurai.com.br</a>
+        </Data>
+      </Inner>
     </Container>
   )
 }
