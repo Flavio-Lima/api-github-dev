f862c0d79cd3d6438e5fd88ecdf56211f90a9d87
Flavio Lima <flavio.lima981@gmail.com>
2023-05-02 09:18:44 -0300

    adiciona estruta para receber dados no componente profile

--- src/pages/RepositoriesPage/Profile/index.jsx
+++ src/pages/RepositoriesPage/Profile/index.jsx
@@ -1,41 +1,62 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import {MdGroup, MdLocationCity, MdWork, MdLink} from 'react-icons/md'
 
 import {Container, Header, Avatar, Login, Name, Inner, Data} from './styles'
 
-function Profile() {
+function Profile({user}) {
   return (
     <Container>
       <Header>
-        <Avatar src='https://avatars.githubusercontent.com/u/76967245?v=4'  alt="Avatar do usuário"/>
-        <Login>flaviolima</Login>
-        <Name>Flávo Lima</Name>
+        <Avatar src={user.avatar_url}  alt="Avatar do usuário"/>
+        <Login>{user.login}</Login>
+        <Name>{user.name}</Name>
       </Header>
 
       <Inner>
         <Data>
           <MdGroup size={20} />
-          30&nbsp;<i>seguidores</i>&nbsp;&middot;&nbsp;10&nbsp;<i>seguindo</i>
+          {user.following}&nbsp;<i>seguidores</i>&nbsp;&middot;&nbsp;{user.followers}&nbsp;<i>seguindo</i>
         </Data>
 
-        <Data>
-          <MdWork size={20} />
-          Odisseia do programador
-        </Data>
+        {user.company && (
+          <Data>
+            <MdWork size={20} />
+            {user.company}
+          </Data>
+        )}
 
-        <Data>
-          <MdLocationCity size={20} />
-          Nova Friburgo
-        </Data>
+        {user.location && (
+          <Data>
+            <MdLocationCity size={20} />
+            {user.location}
+          </Data>
+        )}
 
-        <Data>
-          <MdLink size={20} />
-          <a href="https://devsamurai.com.br/">devsamurai.com.br</a>
-        </Data>
+        {user.blog && (
+          <Data>
+            <MdLink size={20} />
+            <a href={`\\${user.blog}`}>{user.blog}</a>
+          </Data>
+        )}
       </Inner>
     </Container>
-  )
+  );
 }
+
+  Profile.propTypes = {
+    user: PropTypes.shape({
+      login: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      avatar_url: PropTypes.string.isRequired,
+      followers: PropTypes.number.isRequired,
+      following: PropTypes.number.isRequired,
+      company: PropTypes.string,
+      blog: PropTypes.string,
+      location: PropTypes.string,
+    }).isRequired,
+  }
+
 
 export default Profile
--- src/pages/RepositoriesPage/index.jsx
+++ src/pages/RepositoriesPage/index.jsx
@@ -7,10 +7,21 @@ import Repositories from './Repositories';
 import {Container, Sidebar, Main} from "./styles"
 
 function RepositoriesPage() {
+  const user = {
+    login: 'Flavio-Lima',
+    name: null,
+    avatar_url: 'https://avatars.githubusercontent.com/u/76967245?v=4"',
+    followers: 1,
+    following: 4,
+    company: null,
+    blog: null,
+    location: null,
+  }
+
   return (
     <Container>
       <Sidebar>
-        <Profile />
+        <Profile user={user} />
         <Filter />
       </Sidebar>
       <Main>
