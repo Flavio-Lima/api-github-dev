422f1d65ab3d5c3df4605c49d90cdcbf34dd429e
Flavio Lima <flavio.lima981@gmail.com>
2023-05-03 10:37:26 -0300

    adiciona componente repositories funcional

--- src/pages/RepositoriesPage/Repositories/Repository/index.jsx
+++ src/pages/RepositoriesPage/Repositories/Repository/index.jsx
@@ -1,18 +1,29 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import {Container, Name, Description, Footer, Lang, Link} from './Styles';
 
-function Repository() {
+function Repository({ repository }) {
   return (
     <Container color='#F1C419'>
-      <Name> Repository Name</Name>
-      <Description>Repository Description</Description>
+      <Name>{repository.name}</Name>
+      <Description>{repository.description}</Description>
       <Footer color='#F1C419'>
-        <Lang>Repository Lange</Lang>
-        <Link href='https://devsamurai.com.br/' target='_blank'>Ver</Link>
+        <Lang>{repository.language}</Lang>
+        <Link href={repository.html_url} target='_blank'>Ver</Link>
       </Footer>
     </Container>
   )
+}
+
+Repository.propTypes = {
+  repository: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    html_url: PropTypes.string.isRequired,
+    language: PropTypes.string,
+  }).isRequired,
 }
 
 export default Repository
--- src/pages/RepositoriesPage/index.jsx
+++ src/pages/RepositoriesPage/index.jsx
@@ -12,47 +12,53 @@ import { getLangsFrom } from '../../services/api';
 function RepositoriesPage() {
   const user = {
     login: 'Flavio-Lima',
-    name: null,
+    name: 'Flávio Lima',
     avatar_url: 'https://avatars.githubusercontent.com/u/76967245?v=4"',
     followers: 1,
     following: 4,
     company: null,
     blog: null,
-    location: null,
+    location: 'Nova Friburgo - RJ',
   }
 
   const repositories = [
     {
+      id: '1',
       name: 'api-github-dev',
       description: 'Descrição',
       html_url: 'https://github.com/Flavio-Lima/api-github-dev',
       language: 'JavaScript',
     },
     {
+      id: '2',
       name: 'Meu_primeiro_projeto',
       description: 'Primeiro site que desenvolvi após algumas semanas de estudos em um curso rápido que havia iniciado a alguns anos atrás.',
       html_url: 'https://github.com/Flavio-Lima/Meu_primeiro_projeto',
       language: 'HTML',
     },
     {
+      id: '3',
       name: 'password_generator',
       description: 'Descrição',
       html_url: 'https://github.com/Flavio-Lima/password_generator',
       language: 'CSS',
     },
     {
+      id: '4',
       name: 'TODO_List',
       description: 'Descrição',
       html_url: 'https://github.com/Flavio-Lima/TODO_List',
       language: 'TypeScript',
     },
     {
+      id: '5',
       name: 'Projeto_a-ougue',
       description: 'Descrição',
       html_url: 'https://github.com/Flavio-Lima/Projeto_a-ougue',
       language: 'CSS',
     },
     {
+      id: '6',
       name: 'VendaAutomovel',
       description: 'Descrição',
       html_url: 'https://github.com/Flavio-Lima/VendaAutomovel',
