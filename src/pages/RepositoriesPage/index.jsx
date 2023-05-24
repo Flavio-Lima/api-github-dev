90452c16db391857d40af5b1def395aa2c0145d5
Flavio Lima <flavio.lima981@gmail.com>
2023-05-24 10:22:20 -0300

    adiciona consulta de user e repos do Mock da API

--- src/pages/RepositoriesPage/index.jsx
+++ src/pages/RepositoriesPage/index.jsx
@@ -1,78 +1,48 @@
 /* eslint-disable no-console */
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 import Profile from './Profile';
 import Filter from './Filter';
 import Repositories from './Repositories';
 
-import {Container, Sidebar, Main} from "./styles"
+import {Loading, Container, Sidebar, Main} from "./styles"
 
-import { getLangsFrom } from '../../services/api';
+import { getUser, getRepos, getLangsFrom } from '../../services/api';
 
 function RepositoriesPage() {
+  const [user, setUser] = useState();
+  const [repositories, setRepositories] = useState();
+  const [languages, setLanguages] = useState();
   const [currentLanguage, setCurrentLanguage] = useState();
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const loadData = async () => {
+      // getUser();
 
-  const user = {
-    login: 'Flavio-Lima',
-    name: 'Flávio Lima',
-    avatar_url: 'https://avatars.githubusercontent.com/u/76967245?v=4"',
-    followers: 1,
-    following: 4,
-    company: null,
-    blog: null,
-    location: 'Nova Friburgo - RJ',
-  }
+      const [userResponse, repositoriesResponse] = await Promise.all([
+        getUser('Flavio-Lima'),
+        getRepos('Flavio-Lima')
+      ])
 
-  const repositories = [
-    {
-      id: '1',
-      name: 'api-github-dev',
-      description: 'Descrição',
-      html_url: 'https://github.com/Flavio-Lima/api-github-dev',
-      language: 'JavaScript',
-    },
-    {
-      id: '2',
-      name: 'Meu_primeiro_projeto',
-      description: 'Primeiro site que desenvolvi após algumas semanas de estudos em um curso rápido que havia iniciado a alguns anos atrás.',
-      html_url: 'https://github.com/Flavio-Lima/Meu_primeiro_projeto',
-      language: 'HTML',
-    },
-    {
-      id: '3',
-      name: 'password_generator',
-      description: 'Descrição',
-      html_url: 'https://github.com/Flavio-Lima/password_generator',
-      language: 'CSS',
-    },
-    {
-      id: '4',
-      name: 'TODO_List',
-      description: 'Descrição',
-      html_url: 'https://github.com/Flavio-Lima/TODO_List',
-      language: 'TypeScript',
-    },
-    {
-      id: '5',
-      name: 'Projeto_a-ougue',
-      description: 'Descrição',
-      html_url: 'https://github.com/Flavio-Lima/Projeto_a-ougue',
-      language: 'CSS',
-    },
-    {
-      id: '6',
-      name: 'VendaAutomovel',
-      description: 'Descrição',
-      html_url: 'https://github.com/Flavio-Lima/VendaAutomovel',
-      language: 'HTML',
-    },
-  ]
+      setUser(userResponse.data);
+      setRepositories(repositoriesResponse.data);
 
-  const languages = getLangsFrom(repositories)
+      setLanguages(getLangsFrom(repositoriesResponse.data));
+
+      setLoading(false)
+    };
+
+    loadData();
+  }, []);
 
   const onFilterClick = (language) => {
     setCurrentLanguage(language);
   };
+
+  if(loading)
+  {
+    return <Loading>Carregando...</Loading>
+  }
 
   return (
     <Container>
